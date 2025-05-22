0eaf2892ba599a497a76f2b1032b027fb90f7b20
newton689878 <danilacurbakov@gmail.com>
2025-05-22 10:43:41 +0300

    Add files via upload

--- стр 17 зад 2 номер 5.php
+++ стр 17 зад 2 номер 5.php
@@ -1,29 +1,10 @@
 <?php
-  
-// PHP program to reverse
-// digits of a number
-$rev_num = 0;
-$base_pos = 1;
+$N = (int)readline("Введите трехзначное число: ");
 
-/* Recursive function to
-reverse digits of num*/
-function reversDigits($num) {
-    global $rev_num;
-    global $base_pos;
-    if($num > 0) {
-        reversDigits((int)($num / 10));
-        $rev_num += ($num % 10) *
-                    $base_pos;
-        $base_pos *= 10;
-    }
-    return $rev_num;
+if ($N >= 100 && $N <= 999) {
+    $reversedNumber = ($N % 10) * 100 + ((int)($N / 10) % 10) * 10 + (int)($N / 100);
+    echo "Число в обратном порядке: " . $reversedNumber . "\n";
+} else {
+    echo "Ошибка: введено не трехзначное число.\n";
 }
-
-// Driver Code
-$num = 356;
-echo $num;
-echo "\r\n";
-echo 
-    reversDigits($num);
-
 ?>
